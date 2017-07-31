343195205fff26f3d2a2ce28e5a108377e1c2f46
root <celfons>
2017-07-31 09:08:45 -0300

    first

--- src/test/java/com/poi/test/PoiTest.java
+++ src/test/java/com/poi/test/PoiTest.java
@@ -1,16 +1,19 @@
-package com.poi;
+package com.poi.test;
 
 import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.poi.app.Application;
 import com.poi.model.Poi;
 import com.poi.service.PoiService;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {Application.class})
 public class PoiTest {
 
 	@Autowired
